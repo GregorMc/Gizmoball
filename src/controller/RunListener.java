e2556a38ec16bb2844899f981821ff74b6881d18
Ruairi Lukins CS2016 <ruairi.lukins.2016@uni.strath.ac.uk>
2019-03-13 18:34:33 +0000

    Update src/controller/RunListener.java

---
+++ src/controller/RunListener.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import model.Model;
+import view.RunGui;
+
+/**
+ * @author Murray Wood Demonstration of MVC and MIT Physics Collisions 2014
+ */
+
+public class RunListener implements ActionListener {
+
+    private Timer timer;
+    private Model model;
+    private RunGui GUI;
+
+    public RunListener(Model m, RunGui gui) {
+        this.GUI = gui;
+        model = m;
+        timer = new Timer(50, this);
+    }
+
+    @Override
+    public final void actionPerformed(final ActionEvent e) {
+
+        if (e.getSource() == timer) {
+            System.out.println("tick");
+            model.moveBall();
+            model.moveFlippers();
+        } else
+            switch (e.getActionCommand()) {
+                case "Start":
+                    timer.start();
+                    break;
+                case "Stop":
+                    timer.stop();
+                    break;
+                case "Load":
+                    timer.stop();
+                    System.out.println("Loading Action triggered.");
+                    model.loadBoard();
+                    System.out.println("-----------------");
+                    break;
+                case "Run Mode":
+                    System.out.println("Run Mode Clicked");
+                    GUI.switchToRun();
+                    break;
+                case "Build Mode":
+                    timer.stop();
+                    System.out.println("Run Mode Clicked");
+                    GUI.switchToBuild();
+                    break;
+            }
+    }
+}
