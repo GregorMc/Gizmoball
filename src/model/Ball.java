970a0b9a8fbcec564fedc46dcdbbb670136453cd
Ruairi Lukins CS2016 <ruairi.lukins.2016@uni.strath.ac.uk>
2019-03-13 18:34:59 +0000

    Update src/model/Ball.java

---
+++ src/model/Ball.java
@@ -0,0 +1,92 @@
+package model;
+
+import java.awt.Color;
+
+import physics.Circle;
+import physics.Vect;
+
+/**
+ * @author Murray Wood Demonstration of MVC and MIT Physics Collisions 2014
+ */
+
+public class Ball {
+
+    private Vect velocity;
+    private double radius;
+    private double xpos;
+    private double ypos;
+    private Color colour;
+    private final int L = 25;
+    private String id;
+
+    private boolean stopped;
+
+    // x, y coordinates and x,y velocity fixme Need to be in L's
+    public Ball(String ballid, double x, double y, double xv, double yv) {
+        this.id = ballid;
+        xpos = x; // Centre coordinates
+        ypos = y;
+        colour = Color.BLUE;
+        velocity = new Vect(xv, yv);
+        radius = 7;
+        stopped = false;
+    }
+
+    public Vect getVelo() {
+        return velocity;
+    }
+
+    public void setVelo(Vect v) {
+        velocity = v;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public Circle getCircle() {
+        return new Circle((xpos*L)+radius, (ypos*L)+radius, radius);
+    }
+
+    //Ball specific methods that deal with double precision.
+    public double getExactX() {
+        return xpos;
+    }
+
+    public double getExactY() {
+        return ypos;
+    }
+
+    public double getXinPixels(){
+        return xpos*L;
+    }
+
+    public double getYinPixels(){
+        return ypos*L;
+    }
+
+    public void setExactX(double x) {
+        xpos = x;
+    }
+
+    public void setExactY(double y) {
+        ypos = y;
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+
+    public void start() {
+        stopped = false;
+    }
+
+    public boolean stopped() {
+        return stopped;
+    }
+
+    public Color getColour() {
+        return colour;
+    }
+
+}
